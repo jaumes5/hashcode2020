c9c02d602c9d96018e1e864a410fe35a7393aed8
SESA560403 <gauthier.husson@non.se.com>
2020-02-20 20:07:58 +0100

    score

---
+++ gauthier.py
@@ -0,0 +1,15 @@
+def score(libraries, books_score):
+    with open(libraries, "r") as file:
+        data = file.read()
+    data = data.split("\n")
+    A = int(data[0])
+    score = 0
+    scanned_books = set()
+    for i in range(A):
+        Y, K = [int(i) for i in data[1+2*i].split(" ")]
+        books = [int(i) for i in data[2*(i+1)].split(" ")]
+        for book in books:
+            if book not in scanned_books:
+                score += books_score[book]
+                scanned_books.add(book)
+    return score
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from jaime import *
-
+from gauthier import *
 
 if __name__ == "__main__":
     B, L, D, books_score, library = parser("files/f_libraries_of_the_world.txt")
     generate_output(library, books_score, D)
+    score = score('files/output.txt', books_score)
     pass
