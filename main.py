321cb7632320d845d341dd15636ba8b16094fc1f
SESA560403 <gauthier.husson@non.se.com>
2020-02-20 20:22:23 +0100

    score

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from jaime import *
 from gauthier import *
 
 if __name__ == "__main__":
-    B, L, D, books_score, library = parser("files/f_libraries_of_the_world.txt")
-    generate_output(library, books_score, D)
+    B, L, D, books_score, library = parser("files/a_example.txt")
+    #generate_output(library, books_score, D)
     score = score('files/output.txt', books_score)
+    print(score)
     pass
